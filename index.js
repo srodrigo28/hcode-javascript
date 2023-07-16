2880101fdd6e7f3ee4622a23c21afc524dd6e553
Sebastiao Rodrigo <rodrigoexer1@gmail.com>
2023-07-16 18:19:42 -0300

    updated

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-var user = document.querySelector('#exampleInputName')
-
-console.log(user)
+var name = document.querySelector("#exempleInputName") = "Sebastiao"
+var gender = document.querySelector("#form-user-create [name=gender]:checked")
+var birth = document.querySelector("#exempleInputBirth")
+var country = document.querySelector("#exempleInputCountry")
+var email = document.querySelector("#exempleInputEmail")
+var password = document.querySelector("#exampleInputPassword")
+var photo = document.querySelector("#InputFile")
+var admin = document.querySelector("#")
