047050a4290cfa25c49f8dfb54d3c5f000b464cf
Sebastiao Rodrigo <rodrigoexer1@gmail.com>
2023-07-16 22:19:50 -0300

    created-function-image

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,9 +10,36 @@ class UserController{
 
         this.formEl.addEventListener("submit", event => {
             event.preventDefault()
+
+            let values = this.getValues()
+
+            values.photo = "";
             
-            this.addLine(this.getValues())
+            this.getPhoto((content)=>{
+                values.photo = content;
+
+                this.addLine(values)
+            });
         })
+    }
+
+    getPhoto(callback){
+        let fileReader = new FileReader();
+
+        let elements = [...this.formEl.elements].filter(item => {
+            if(item.name === 'photo'){
+                return item
+            }
+        });
+
+        let file = elements[0].files[0];
+
+        fileReader.onload = () => {
+            
+            callback(fileReader.result)
+        };
+
+        fileReader.readAsDataURL(file);
     }
 
     getValues() {
@@ -39,16 +66,14 @@ class UserController{
         user.photo, 
         user.admin
     )
-}
+    }
 
-addLine(dataUser){
-
-        console.log(dataUser)
+    addLine(dataUser){
     
         this.tableE1.innerHTML = `
             <tr>
                 <td>
-                    <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
+                    <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm">
                 </td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
